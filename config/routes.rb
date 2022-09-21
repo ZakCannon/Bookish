c6031989b84c8b24fe2755c2e5f305a3d8ef7bcd
ZakCannon <Zak.Cannon@softwire.com>
2022-09-21 14:41:18 +0100

    fixing devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :books
   resources :copies
 
   get 'books/:id/copy_edit', to: 'books#copy_edit'
 
-  root "books#index"
+  root to: "books#index"
 end
